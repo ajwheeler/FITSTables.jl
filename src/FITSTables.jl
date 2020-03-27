43c8658a3b5a534bdb0edfb30323a144f731d372
Adam Wheeler <a.wheeler@columbia.edu>
2020-03-27 12:08:12 -0400

    make multidimensional arrays work?

--- src/FITSTables.jl
+++ src/FITSTables.jl
@@ -12,7 +12,17 @@ Tables.istable(::Type{<:THDU}) = true
 Tables.columnaccess(::Type{<:THDU}) = true
 Tables.columns(t::THDU) = t
 Tables.columnnames(t::THDU) = Symbol.(FITSIO.colnames(t))
-Tables.getcolumn(t::THDU, i::Int) = FITSIO.read(t, FITSIO.colnames(t)[i])
-Tables.getcolumn(t::THDU, s::Symbol) = FITSIO.read(t, String(s))
+
+"""reshape multidimensional array into array of (possibly multidimensional) arrays"""
+function collapse(col::Array)
+    dim = length(size(col))
+    if dim == 1
+        col
+    else
+        [view(col, vcat(repeat([:], dim-1), i)...) for i in 1:size(col, dim)]
+    end
+end
+Tables.getcolumn(t::THDU, i::Int) = collapse(FITSIO.read(t, FITSIO.colnames(t)[i]))
+Tables.getcolumn(t::THDU, s::Symbol) = collapse(FITSIO.read(t, String(s)))
 
 end # module
